8d1d410a3068a4faac25c2c0d0cb1e5df12db4ad
Luigi213 <reyesgianluigi88@gmail.com>
2023-01-23 18:51:29 +0100

    add the components appsecotionsmart

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,17 +1,18 @@
 <script>
 import Jumbo from './Jumbo.vue';
+import AppSectionSmart from './AppSectionSmart.vue'
 export default {
     components:{
         Jumbo,
+        AppSectionSmart,
     }
 }
 </script>
 <template lang="">
     <main>
         <Jumbo/>
+        <AppSectionSmart/>
     </main>
 </template>
-<style lang="scss" scoped>
-    @use '../styles/partials/variables' as *;
-    @use '../styles/partials/mixins' as *;
+<style lang="scss" scoped>    
 </style>
---
+++ src/components/AppSectionSmart.vue
@@ -0,0 +1,61 @@
+<script>
+export default {
+    
+}
+</script>
+<template lang="">
+    <section class="Animetion">
+        <div class="container">
+            <div class="row">
+                <div class="col-9 offset-3 text-center">
+                    <div class="row">
+                        <div class="col-10">
+                            <h5 class="fw-semibold fs-4">Our Love For Animation</h5>
+                            <h1 class="fw-semibold">We're Storytelling Experts With Creative Smarts</h1>
+                            <p>Our culture inspires the creative and innovative spirit found throughout our Studio. We breathe life into new ideas!</p>                                
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="smart">
+            <img src="../assets/images/smarttwo.png" alt="smarttwo">
+        </div>
+    </section>
+</template>
+<style lang="scss" scoped> 
+    @use '../styles/partials/variables' as *;
+    @use '../styles/partials/mixins' as *;
+    .Animetion{
+        padding-top: 130px ;
+        position: relative;        
+        .smart{            
+            width: 750px;
+            height: 1000px;
+            position: absolute;
+            top:0;
+            left: -15%;
+            img{
+                border-radius: 100%;
+                object-fit: cover;
+                width: 100%;
+                height: 100%;
+            }
+        }
+        .container{
+            .row{
+                h5{
+                    color: $third;
+                }
+                h1{
+                    font-size: 60px;
+                }
+                p{
+                    width: 75%;
+                    font-size: 17px ;
+                    margin: 0 auto;
+                }
+            }
+        }
+    }
+</style>
