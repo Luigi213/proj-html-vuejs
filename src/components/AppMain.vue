c71d3ccc34c40891ab81aa344695087deb6abd12
Luigi213 <reyesgianluigi88@gmail.com>
2023-01-23 22:12:29 +0100

    fixed the writing and add the new component sectio

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,21 +3,24 @@ import SectionJumbo from './SectionJumbo.vue';
 import SectionSmart from './SectionSmart.vue'
 import SectionStats from './SectionStats.vue'
 import SectionFeatures from './SectionFeatures.vue'
+import SectionProcess from './SectionProcess.vue'
 export default {
     components:{
         SectionJumbo,
         SectionSmart,
         SectionStats,
         SectionFeatures,
+        SectionProcess,
     }
 }
 </script>
 <template lang="">
     <main>
         <SectionJumbo/>
-        <SectionSectionSmart/>
+        <SectionSmart/>
         <SectionStats/>
         <SectionFeatures/>
+        <SectionProcess/>
     </main>
 </template>
 <style lang="scss" scoped>    
